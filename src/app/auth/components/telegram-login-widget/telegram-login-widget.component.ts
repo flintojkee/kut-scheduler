eb06f1ac80b3f5c4d1565705f2b35c9a26aa0df2
Денис Василенко <denisvasilenko@MacBook-Pro-Denis.local>
2019-10-09 17:56:50 +0300

    fix

--- src/app/auth/components/telegram-login-widget/telegram-login-widget.component.ts
+++ src/app/auth/components/telegram-login-widget/telegram-login-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { environment } from '@root/environments/environment';
 
 @Component({
@@ -6,9 +6,12 @@ import { environment } from '@root/environments/environment';
   templateUrl: './telegram-login-widget.component.html',
   styleUrls: ['./telegram-login-widget.component.scss']
 })
-export class TelegramLoginWidgetComponent implements AfterViewInit {
+export class TelegramLoginWidgetComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('script', {static: true}) script: ElementRef;
+  constructor(private ngZone: NgZone) {
+
+  }
+  @ViewChild('script', { static: true }) script: ElementRef;
 
   convertToScript() {
     const element = this.script.nativeElement;
@@ -21,17 +24,32 @@ export class TelegramLoginWidgetComponent implements AfterViewInit {
     script.setAttribute('data-request-access', 'write');
     element.parentElement.replaceChild(script, element);
   }
-//   <script
-//   async
-//   src="https://telegram.org/js/telegram-widget.js?7"
-//   data-telegram-login="KutSchedulerBot"
-//   data-size="large"
-//   data-radius="20"
-//   data-auth-url="https://kut-scheduler-api.herokuapp.com/api/v1/login"
-//   data-request-access="write"
-// ></script>
+  ngOnInit() {
+    window['loginViaTelegram'] = (loginData) => this.loginViaTelegram(loginData);
+  }
+  private loginViaTelegram(loginData: TelegramLoginData) {
+    this.ngZone.run(() => console.log(loginData));
+  }
+  //   <script
+  //   async
+  //   src="https://telegram.org/js/telegram-widget.js?7"
+  //   data-telegram-login="KutSchedulerBot"
+  //   data-size="large"
+  //   data-radius="20"
+  //   data-auth-url="https://kut-scheduler-api.herokuapp.com/api/v1/login"
+  //   data-request-access="write"
+  // ></script>
   ngAfterViewInit() {
     this.convertToScript();
   }
+}
 
+export interface TelegramLoginData {
+  id: string;
+  first_name?: string;
+  last_name?: string;
+  username?: string;
+  photo_url?: string;
+  auth_date: string;
+  hash: string;
 }
--- src/app/auth/shared/telegram-login.service.ts
+++ src/app/auth/shared/telegram-login.service.ts
@@ -1,27 +1,27 @@
-import { Injectable, NgZone } from '@angular/core';
+// import { Injectable, NgZone } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class TelegramLoginService {
-  constructor(private ngZone: NgZone) {
-  }
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class TelegramLoginService {
+//   constructor(private ngZone: NgZone) {
+//   }
 
-  init() {
-    window['loginViaTelegram'] = loginData => this.loginViaTelegram(loginData);
-  }
+//   init() {
+//     window['loginViaTelegram'] = loginData => this.loginViaTelegram(loginData);
+//   }
 
-  private loginViaTelegram(loginData: TelegramLoginData) {
-    this.ngZone.run(() => console.log());
-  }
-}
+//   private loginViaTelegram(loginData: TelegramLoginData) {
+//     this.ngZone.run(() => console.log());
+//   }
+// }
 
-export interface TelegramLoginData {
-  id: string;
-  first_name?: string;
-  last_name?: string;
-  username?: string;
-  photo_url?: string;
-  auth_date: string;
-  hash: string;
-}
+// export interface TelegramLoginData {
+//   id: string;
+//   first_name?: string;
+//   last_name?: string;
+//   username?: string;
+//   photo_url?: string;
+//   auth_date: string;
+//   hash: string;
+// }
